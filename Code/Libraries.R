5a20f7501eb6f1571187c57dfc885cc4af19402d
Toni Vivó Pons <94048441+ToniVP@users.noreply.github.com>
2023-09-13 16:55:59 +0200

    Update Libraries.R

--- Code/Libraries.R
+++ Code/Libraries.R
@@ -6,6 +6,9 @@ listoflibrary<-c("data.table","vegan","utils","tidyr","stats","grid", "doParalle
                  "ggsci","ggplot2","ggrepel","ggpubr","funrar","foreach","dplyr", "raster", "mgcViz", "gridExtra",
                  "sp", "rangeBuilder", "colorspace", "automap", "ape", "seegSDM", "memisc", "mgcv", "ggnewscale", "viridis")
 
+#seegSDM not available in CRAN
+devtools::install_github('SEEG-Oxford/seegSDM')
+
 for (pkg in listoflibrary){
   if(!eval(bquote(require(.(pkg))))) {eval(bquote(install.packages(.(pkg))))}
   eval(bquote(library(.(pkg))))
